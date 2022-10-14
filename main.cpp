da2d96be9a6df73fb61054e5e7f4287cb02daa2b
Quentin Couland <couland-q@ird222-07-l.etu.polytech.univ-nantes.prive>
2022-10-14 16:16:01 +0200

    More testing

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 // <addr> <opcode> <mnemonic> [<operand> ...] [; commentary ]
 
+// TODO: endian is wrong (0x152 operand value should be 0xfffe, is 0xfeff for now)
+
 #include <iostream>
 
 #include "include/instruction_list.hpp"
@@ -9,10 +11,24 @@
 
 int main() {
     std::cout << "Hello world!" << std::endl;
+
+    // Loading opcodes
     InstructionsList instr_lst;
     instr_lst.readJSONtoStruct("../opcodes.json");
+
+    // Loading the cartridge (= opening the stream)
     CartReader cart_reader;
-    cart_reader.readData("../res/Boxes.gb");
+    cart_reader.init("../res/snake.gb");
+    cart_reader.readMetaData();
     cart_reader.meta.displayMetaData();
+    cart_reader.checkNintendoLogo(true);
+
+    uint address = 0x150;
+    while(address < 0x180) {
+        address = cart_reader.decodeInstr(address, instr_lst);
+    }
+
+    cart_reader.stop();
+
     return 0;
 }
